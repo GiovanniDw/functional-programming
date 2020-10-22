3c661a550331f2ce05a9984865adc4c45399f9a8
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2020-10-22 16:49:59 +0200

    json data is now showing in console.log

--- exercise/index.js
+++ exercise/index.js
@@ -1,9 +1,66 @@
+const data = '../data/survey_id.json';
+
+// function getData(data) {
+// 	let getData = fetch(data)
+// 		.then((response) => response.json())
+// 		.then(json => console.log(json))
+// 		// .then((result) => {
+// 		// 	console.log(result);
+// 		// 	return result
+// 		// })
+// 		.catch((error) => {
+// 			console.error('nope' + error);
+// 		});
+// };
+
+status = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+        console.log(response)
+		return Promise.resolve(response);
+	} else {
+		return Promise.reject(new Error(response.statusText));
+	}
+}
+
+json = (response) => {
+	return response.json();
+}
+
+surveyAnswers = (response) => { 
+    return 
+}
 
 
-fetch('../excercise/data/survey_id.json').then((response) => {
-    return response.json();
-}).then((obj) => {
-    console.log(obj);
-}).catch((error) => {
-    console.error('nope')
-})
+
+let getData = fetch(data)
+    .then(status)
+    .then(json)
+    .then(data => {
+        console.log('Request succeeded with JSON response', data)
+        return data
+    })
+    .then(data => {
+
+    })
+    // .then((json) => {
+    //     console.log(json)
+    //     return json
+    // })
+	// .then((json) => {
+    //     console.log(json);
+        
+	// 	return json
+	// })
+	.catch((error) => {
+		console.error('nope' + error);
+	});
+
+var surveyAnswers = JSON.stringify(getData);
+
+// let surveyAnswers = getData;
+
+console.log(json());
+
+let getAge = (getData) => {
+
+}
