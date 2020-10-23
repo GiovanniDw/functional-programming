6c5dc13a4d7ccf0a82e8f657b5c2464752322683
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2020-10-23 02:27:35 +0200

    added some functionalities

--- exercise/index.js
+++ exercise/index.js
@@ -1,4 +1,5 @@
-const data = '../data/survey_id.json';
+// const data = '../data/survey_id.json';
+const data = '../data/Survey_Information_Design_clean-parsed.json';
 
 responseStatus = (response) => {
     if (response.status >= 200 && response.status < 300) {
@@ -14,48 +15,70 @@ readResponseJSON = (response) => {
 }
 
 logResult = (result) => {
+    JSON.stringify(result)
     console.log(result)
+    return result
 }
+
+calcSiblings = (result) => {
+    for (let i = 0; i < result.length; i++) {
+        let getBrothers = result[i].hoeveelheidBroers;
+		let getSisters = result[i].hoeveelheidZussen;
+        let siblings = getBrothers + getSisters
+        console.log(siblings)
+    }
+    return
+    // console.log(siblings)
+};
+
+//thanks to https://stackoverflow.com/a/34014512/13772119
+
+stringToNumber = (result) => {
+ for (let i = 0; i < result.length; i++) {
+		let obj = result[i];
+		for (let prop in obj) {
+			if (obj.hasOwnProperty(prop) && !isNaN(obj[prop])) {
+				obj[prop] = +obj[prop];
+			}
+		}
+ }
+    return result
+}
+
+
 
 fetchJSON = (data) => {
     fetch(data)
-        .then(responseStatus)
-        .then(readResponseJSON)
-        .then(logResult)
-        .then(data => {
-            console.log('Request succeeded with JSON response', data)
-            return getAllSurveyData(data);
-        })
-        // .then(data => {
-
-        // })
-        // .then((json) => {
-        //     console.log(json)
-        //     return json
-        // })
-        // .then((json) => {
-        //     console.log(json);
-        
-        // 	return json
-        // })
-        .catch((error) => {
-            console.error('nope' + error);
-        });
+		.then(responseStatus)
+		.then(readResponseJSON)
+		.then(logResult)
+		.then(stringToNumber)
+		.then(calcSiblings)
+		// .then(getAge)
+		.catch((error) => {
+			console.error('nope' + error);
+		});
 }
 
 fetchJSON(data)
 
-// var surveyAnswers = JSON.stringify(getData);
 
-// let surveyAnswers = getData;
+// getAge = (result) => {
+// 	for (let i = 0; i < result.length; i++) {
+// 		let getBirthday = result[i].geboortedatum;
+//         console.log(getBirthday);
+//         calcAge(getBirthday)
+// 	}
+// };
+
+// let calcAge = (birthday) => {
+//     let today = new Date().getTime()
+//     let birthDate = new Date(birthday)
 
 
-getAllSurveyData = (data) => {
-	for (let i = 0; i < data.length; i++) {
-		const surveyAnswers = data[i];
-		console.log(surveyAnswers);
-	}
-};
- 
 
- 
+//     // let ageDif = Date.parse(birthday)
+//     console.log(birthDate)
+//     // let ageDate = new Date(ageDif)
+//     // return Math.abs(ageDate.getUTCFullYear() - 1970);
+// }
