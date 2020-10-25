8398eab15769a4afabdb4f5c362045a3a1fda034
GiovanniDw <giovannidwidjosewojo@hotmail.com>
2020-10-25 02:01:50 +0100

    added some functions and reorder functions

--- exercise/index.js
+++ exercise/index.js
@@ -1,91 +1,97 @@
-// const data = '../data/survey_id.json';
 const data = '../data/Survey_Information_Design_clean-parsed.json';
 
-responseStatus = (response) => {
-    if (response.status >= 200 && response.status < 300) {
-        console.log(response)
+fetch(data)
+	.then(responseStatus)
+	.then(readResponseJSON)
+	.then(logResult)
+	.then(stringToNumber)
+	// .then(calcSiblings)
+	.then(correctBirthDates)
+	.catch((error) => {
+		console.error('Can not fetch becasuse: ' + error);
+	});
+
+function responseStatus(response) {
+	if (response.status >= 200 && response.status < 300) {
+		console.log(response);
 		return Promise.resolve(response);
 	} else {
 		return Promise.reject(new Error(response.statusText));
 	}
-}
+};
 
-readResponseJSON = (response) => {
+function readResponseJSON(response) {
 	return response.json();
-}
+};
 
-logResult = (results) => {
-    
-    JSON.stringify(results)
-    console.log(results)
-    return results
-}
+function logResult(result) {
+	console.log(result);
+	return result;
+};
 
 //thanks to https://stackoverflow.com/a/34014512/13772119
 
-stringToNumber = (results) => {
-    console.log("str to nm")
-    console.log(results)
- for (let i = 0; i < results.length; i++) {
-		let obj = results[i];
+function stringToNumber(result){
+	console.log('str to nm');
+	console.log(result);
+	for (let i = 0; i < result.length; i++) {
+		let obj = result[i];
 		for (let prop in obj) {
 			if (obj.hasOwnProperty(prop) && !isNaN(obj[prop])) {
 				obj[prop] = +obj[prop];
 			}
 		}
- }
-    return results
-}
-
-
-calcSiblings = (results) => {
-
-    for (let i = 0; i < results.length; i++) {
-        let getBrothers = results[i].hoeveelheidBroers;
-		let getSisters = results[i].hoeveelheidZussen;
-        let siblings = getBrothers + getSisters;
-        console.log(siblings)
-        return results
-    }
-    
-    
+	}
+	return result;
 };
 
-getAnswersByColName = (results, colName) => {
-    if (results.length < 1) {
-        console.log("No Items in result")   
-        return
-    }
-    let resultByColName = []
-    for (result of results) {
-        resultByColName.push(result[colName])
-    }
-    return resultByColName
-    
-}
+function calcSiblings(result) {
+	let siblings = [];
+	for (let i = 0; i < result.length; i++) {
+		let getBrothers = result[i].hoeveelheidBroers;
+		let getSisters = result[i].hoeveelheidZussen;
+		siblings.push(getBrothers + getSisters);
+	}
+	console.log(`Siblings per object: ${siblings}`);
+	return;
+};
 
-getAge = (results) => {
-    console.log("GetAge function")
-    let getBirthdate = getAnswersByColName(results, "geboortedatum");
-    console.log(getBirthdate);
-    let birthdate = convertDateString(getBirthdate);
-    console.log(birthdate)
+function getAnswersByColName(results, colName) {
+	if (results.length < 1) {
+		console.log('No Items in result');
+		return;
+	}
+	let resultByColName = [];
+	for (result of results) {
+		resultByColName.push(result[colName]);
+	}
+	return resultByColName;
+};
 
+// calc age https://stackoverflow.com/questions/4060004/calculate-age-given-the-birth-date-in-the-format-yyyymmdd
 
-}
+function correctBirthDates(results) {
+	console.log('correctBirthDates function');
+	let getBirthdate = getAnswersByColName(results, 'geboortedatum');
+	console.log(getBirthdate);
+	let birthdate = formatDateString(getBirthdate);
+	console.log(birthdate);
+	return results;
+};
 
-convertDateString = (date) => {
-    let convertedDates = [];
-    date.forEach(dateString => {
-        let dateParts = dateString.split('-');
-        let dateObj = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
-        
-        convertedDates.push(dateObj.toString())
-    });
-    return convertedDates
-}
+// Date Transform to https://stackoverflow.com/questions/33299687/how-to-convert-dd-mm-yyyy-string-into-javascript-date-object
+function formatDateString(date) {
+	let convertedDates = [];
+	date.forEach((dateString) => {
+		let dateParts = dateString.split('-');
+		let dateObj = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+
+		convertedDates.push(dateObj.toString());
+	});
+	return convertedDates;
+};
 // convertDateString = (date) => {
-	 
+
 // 	let convertedDates = date.map((dateString) => {
 // 		let dateParts = dateString.split('-');
 // 		let dateObj = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
@@ -93,22 +99,6 @@ convertDateString = (date) => {
 // 	return convertedDates;
 // };
 
-
-
-fetchJSON = (data) => {
-    fetch(data)
-		.then(responseStatus)
-		.then(readResponseJSON)
-		.then(logResult)
-		// .then(stringToNumber)
-		.then(getAge)
-		// .then(calcSiblings)
-		.catch((error) => {
-			console.error('Can not fetch becasuse: ' + error);
-		});
-}
-
-fetchJSON(data)
 
 
 // getAge = (result) => {
@@ -122,8 +112,6 @@ fetchJSON(data)
 // let calcAge = (birthday) => {
 //     let today = new Date().getTime()
 //     let birthDate = new Date(birthday)
-
-
 
 //     // let ageDif = Date.parse(birthday)
 //     console.log(birthDate)
